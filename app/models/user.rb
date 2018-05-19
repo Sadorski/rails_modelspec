f49a059bf6a915bd15910203f91bbc52c2f01592
Sultan Sultani <sadorski@Sultans-MBP.lan>
2018-05-19 10:26:36 -0700

    initial commit

---
+++ app/models/user.rb
@@ -0,0 +1,6 @@
+class User < ActiveRecord::Base
+        validates :first_name, :last_name, :presence => true
+        VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
+        validates :email, presence: true, uniqueness: {case_sensitive: false}, format: { with: VALID_EMAIL_REGEX }
+        
+end
